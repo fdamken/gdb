fb417a09e65e3b362a08f9956c6fa9ec7c3ada2d
Dominik Cech <dominik.cech@lcmanager.org>
2016-04-26 20:48:45 +0200

    Added the basic structure for Spring Web MVC.

---
+++ gdb-web-control/src/main/java/org/lcmanager/gdb/main/GameDatabase.java
@@ -0,0 +1,39 @@
+/*
+ * #%L
+ * Game Database
+ * %%
+ * Copyright (C) 2016 - 2016 LCManager Group
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.lcmanager.gdb.main;
+
+import org.lcmanager.gdb.config.BaseConfiguration;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * The main class of the game database. Initializes Spring.
+ *
+ */
+public class GameDatabase {
+    /**
+     * The main method.
+     *
+     * @param args
+     *            The command-line arguments.
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(BaseConfiguration.class, args);
+    }
+}
